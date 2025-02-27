cfff1801ecd2921e629f8fe9bdb67d3276245e96
marimontefusco <mari.montefusco@gmail.com>
2025-02-27 11:53:08 -0300

    Creating repositories

---
+++ src/main/java/com/application/ims/infrastructure/SaleItemRepository.java
@@ -0,0 +1,9 @@
+package com.application.ims.infrastructure;
+
+import com.application.ims.domain.entity.SaleItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleItemRepository extends JpaRepository<SaleItem, Long> {
+}
