07ef2de80f6ec98322cd3d37af78be5e815709c9
JoaoBenignodev <joao.benigno@aluno.faculdadeimpacta.com.br>
2025-03-02 09:55:15 -0300

    Implementing Category CRUD methods

--- src/main/java/com/application/ims/domain/service/implementation/CategoryServiceImpl.java
+++ src/main/java/com/application/ims/domain/service/implementation/CategoryServiceImpl.java
@@ -1,4 +1,116 @@
 package com.application.ims.domain.service.implementation;
 
-public class CategoryServiceImpl {
+import org.springframework.stereotype.Service;
+import com.application.ims.domain.service.interfaces.CategoryServiceInterface;
+import com.application.ims.infrastructure.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.application.ims.domain.dto.response.CategoryResponseDto;
+import com.application.ims.domain.entity.Category;
+import com.application.ims.domain.dto.request.create.CategoryRequestDto;
+import com.application.ims.domain.dto.request.update.UpdateCategoryRequestDto;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl  implements CategoryServiceInterface {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    // POST method implementation
+    @Override
+    public CategoryResponseDto save(CategoryRequestDto categoryRequestDto) {
+
+        // creating up a new category
+        Category category = new Category();
+
+        // setting up the new category attributes
+        category.setName(categoryRequestDto.getName());
+        category.setDescription(categoryRequestDto.getDescription());
+
+        Category savedCategory = categoryRepository.save(category);
+
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(savedCategory.getId());
+        categoryResponseDto.setName(savedCategory.getName());
+        categoryResponseDto.setDescription(savedCategory.getDescription());
+
+        return categoryResponseDto;
+
+    }
+
+    // GET method implementation
+    @Override
+    public CategoryResponseDto getCategory(Long id) {
+
+        // fetching the category with the given id
+        Category category = categoryRepository.findById(id).orElseThrow(()
+                -> new RuntimeException("Category with id " + id + " not found"));
+
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(category.getId());
+        categoryResponseDto.setName(category.getName());
+        categoryResponseDto.setDescription(category.getDescription());
+
+        return categoryResponseDto;
+    }
+
+    // GET method implementation (list)
+    @Override
+    public List<CategoryResponseDto> getCategorys() {
+
+        // fetching all existing categorys
+        List<Category> categorys = categoryRepository.findAll();
+        return categorys.stream().map(this::categoryResponseDtos).toList();
+
+    }
+
+    private CategoryResponseDto categoryResponseDtos(Category category) {
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(category.getId());
+        categoryResponseDto.setName(category.getName());
+        categoryResponseDto.setDescription(category.getDescription());
+
+        return categoryResponseDto;
+    }
+
+    // GET method implementation
+    @Override
+    public CategoryResponseDto update(Long id, UpdateCategoryRequestDto categoryRequestDto) {
+
+        // fetching the category with the given id
+        Category category = categoryRepository.findById(id).orElseThrow(()
+                -> new RuntimeException("Category with id " + id + " not found"));
+
+        category.setName(categoryRequestDto.getName());
+        category.setDescription(categoryRequestDto.getDescription());
+
+        // updating the category with new given data
+        Category updatedCategory = categoryRepository.save(category);
+
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(updatedCategory.getId());
+        categoryResponseDto.setName(category.getName());
+        categoryResponseDto.setDescription(category.getDescription());
+
+        return categoryResponseDto;
+    }
+
+    // DELETE method implementation
+    @Override
+    public CategoryResponseDto delete(Long id) {
+
+        // fetching the category with the given id
+        Category category = categoryRepository.findById(id).orElseThrow(()
+                -> new RuntimeException("Category with id " + id + " not found"));
+
+        categoryRepository.delete(category);
+
+        return null;
+    }
 }
--- src/main/java/com/application/ims/domain/service/interfaces/CategoryServiceInterface.java
+++ src/main/java/com/application/ims/domain/service/interfaces/CategoryServiceInterface.java
@@ -1,4 +1,15 @@
 package com.application.ims.domain.service.interfaces;
 
+import com.application.ims.domain.dto.request.create.CategoryRequestDto;
+import com.application.ims.domain.dto.request.update.UpdateCategoryRequestDto;
+import com.application.ims.domain.dto.response.CategoryResponseDto;
+
+import java.util.List;
+
 public interface CategoryServiceInterface {
+    CategoryResponseDto save(CategoryRequestDto categoryRequestDto);
+    CategoryResponseDto getCategory(Long id);
+    List<CategoryResponseDto> getCategorys();
+    CategoryResponseDto update(Long id, UpdateCategoryRequestDto categoryRequestDto);
+    CategoryResponseDto delete(Long id);
 }
